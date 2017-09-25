21a670a31b388c46f996e966c56de5bd2687b716
jjwyse <josh@cloud-elements.com>
2017-09-24 18:15:50 -0600

    :fire: can now set date

--- src/components/Input/DatePicker.jsx
+++ src/components/Input/DatePicker.jsx
@@ -5,8 +5,12 @@ import {DatePicker as MaterialDatePicker} from 'redux-form-material-ui';
  * Wraps the standard MaterialUI/ReduxForm DatePicker and allows you to pass in a string as the initial value
  */
 const DatePicker = props => {
-  const shouldCastToDate = props.input.value && typeof props.input.value === 'string';
-  return <MaterialDatePicker {...props} value={shouldCastToDate ? new Date(props.input.value) : null} />;
+  const toSafeValue = value => {
+    if (typeof value === 'string') return new Date(props.input.value);
+    if (typeof value === 'object') return value;
+    return null;
+  };
+  return <MaterialDatePicker {...props} value={toSafeValue(props.input.value)} />;
 };
 DatePicker.propTypes = {
   input: PropTypes.object.isRequired,
